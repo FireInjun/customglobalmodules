ac66a6239c95d3fc8c34dd19ad2c09837f556e70
Mykeal Kenny <mk@fireinjun.com>
2018-06-21 21:20:39 -0400

    added vscode

--- Dev_Modules/startclass.psm1
+++ Dev_Modules/startclass.psm1
@@ -3,6 +3,7 @@
     Start C:\Users\devFireInjun\AppData\Local\slack\Slack.exe
     Start C:\Users\devFireInjun\AppData\Local\SourceTree\SourceTree.exe
     Start C:\Users\devFireInjun\AppData\Roaming\Zoom\bin\Zoom.exe
+    start "C:\Program Files\Microsoft VS Code\Code.exe\"
 }
 
 Export-ModuleMember -function startclass
