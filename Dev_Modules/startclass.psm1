51cb56eac86852d4cea1b152a243973c539e20f2
Mykeal Kenny <mk@fireinjun.com>
2018-06-21 23:44:43 -0400

    completed automern and made startclass run cleaner

--- Dev_Modules/startclass.psm1
+++ Dev_Modules/startclass.psm1
@@ -1,9 +1,9 @@
 ﻿function global:startclass() {
-    Start C:\Users\devFireInjun\AppData\Local\hyper\Hyper.exe 
-    Start C:\Users\devFireInjun\AppData\Local\slack\Slack.exe
-    Start C:\Users\devFireInjun\AppData\Local\SourceTree\SourceTree.exe
-    Start C:\Users\devFireInjun\AppData\Roaming\Zoom\bin\Zoom.exe
-    start "C:\Program Files\Microsoft VS Code\Code.exe\"
+    Start-Process C:\Users\devFireInjun\AppData\Local\hyper\Hyper.exe 
+    Start-Process C:\Users\devFireInjun\AppData\Local\slack\Slack.exe
+    Start-Process C:\Users\devFireInjun\AppData\Local\SourceTree\SourceTree.exe
+    Start-Process C:\Users\devFireInjun\AppData\Roaming\Zoom\bin\Zoom.exe
+    Start-Process "C:\Program Files\Microsoft VS Code\Code.exe\"
 }
 
 Export-ModuleMember -function startclass
